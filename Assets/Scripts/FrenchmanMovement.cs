981c6f1695cb1484fd0cca39a90fee5ca461f415
jesseeerikainen <fjeee001@edu.xamk.fi>
2023-10-06 22:02:35 +0300

    Movement for frenchmen
    Just the basic movement for the Frenchmen to chase the player. Need to make it more linear, and make them dodge the player once close enough.

---
+++ Assets/Scripts/FrenchmanMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+
+public class FrenchmanMovement : MonoBehaviour
+{
+    public string playerObjectName = "Car";
+    public float pullForce = 10f;
+    private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject playerObject = GameObject.Find(playerObjectName);
+            player = playerObject.transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 directionToPlayer = player.position - transform.position;
+            GetComponent<Rigidbody>().AddForce(directionToPlayer.normalized * pullForce, ForceMode.Force);
+        }
+    }
+}
